d96531f7b1bf0728af6244058b564264ba2ea19a
ornella28 <angelnibaruta88@gmail.com>
2026-04-22 10:17:48 +0200

    corrections of index.html , usinge state UI and user state

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
+import { useMemo, useState } from 'react'
 import Header from './components/Header'
 import SidebarFilters from './components/SidebarFilters'
 import ProductSection from './components/ProductSection'
 import Newsletter from './components/Newsletter'
 import Footer from './components/Footer'
+import { products } from './data/products'
 
 function App() {
-  return (
-      <div className="min-h-screen bg-slate-100/30 text-slate-950 antialiased font-sans">
-        <Header />
+    const [searchTerm, setSearchTerm] = useState('')
+    const [selectedCategory, setSelectedCategory] = useState('All')// useState is used to store the search text and selected category so the UI can update when the user interact with it
 
-        <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <div className="grid grid-cols-1 gap-6 lg:grid-cols-12">
-            <SidebarFilters />
-            <ProductSection />
-          </div>
-        </main>
+    const filteredProducts = products.filter((product) => {
+        const matchesSearch = product.title
+            .toLowerCase()
+            .includes(searchTerm.toLowerCase())
 
-        <Newsletter />
-        <Footer />
-      </div>
-  )
+            const matchesCategory =
+                selectedCategory === 'All' || product.category === selectedCategory
+
+            return matchesSearch && matchesCategory
+        })
+
+
+    return (
+        <div className="min-h-screen bg-slate-100/30 text-slate-950 antialiased font-sans">
+            <Header searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+
+            <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                <div className="grid grid-cols-1 gap-6 lg:grid-cols-12">
+                    <SidebarFilters
+                        selectedCategory={selectedCategory}
+                        onCategoryChange={setSelectedCategory}
+                    />
+                    <ProductSection products={filteredProducts} />
+                </div>
+            </main>
+
+            <Newsletter />
+            <Footer />
+        </div>
+    )
 }
 
 export default App
